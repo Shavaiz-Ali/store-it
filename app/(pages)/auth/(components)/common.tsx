4d42a0d890fcd002c5f75d090eed9ba13c87d0fb
Shavaiz-Ali <shavaizali159@gmail.com>
2024-11-13 12:28:00 +0500

    auth routing done and some common cmps done

---
+++ app/(pages)/auth/(components)/common.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import React from "react";
+
+const Common = () => {
+  return (
+    <div className="max-w-[580px] hidden lg:flex flex-col shrink-0 justify-center items-start py-[114px] px-[75px] bg-primaryOrangeLight h-screen overflow-hidden gap-y-[96px]">
+      <div className="flex justify-center items-center gap-x-2">
+        <p className="text-[37px] font-[family-name:var(--font-poppins-medium)] leading-[56px] text-[#ffffff] font-[400]">
+          Storeit
+        </p>
+      </div>
+      <div className="space-y-4">
+        <h1 className="font-[family-name:var(--font-poppins-bold)] text-[46px] leading-[56px] text-[#ffffff] font-[700]">
+          Manage your files the best way
+        </h1>
+        <p className="text-[16px] leading-[24px] font-[family-name:var(--font-poppins-regular)] text-[#ffffff] font-normal">
+          Awesome, we&apos;ve created the perfect place for you to store all
+          your documents.
+        </p>
+      </div>
+      <div className="flex justify-center items-center w-full">
+        <Image
+          src={"/images/auth.png"}
+          alt="auth image"
+          height={242}
+          width={242}
+          className="text-center self-center"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Common;
